e1f0c073d7e0cf0ec4f68fed9ad849511faceecd
billnho51 <billnho1214@gmail.com>
2023-12-01 02:12:45 +1030

    added testing ice cream

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product/Product';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <div>Hello, this is my testing website</div>
+        return <Product/>
       </header>
     </div>
   );
